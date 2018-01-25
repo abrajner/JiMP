de187a14f6d97996a7d4c94c81aed245818419ca
abrajner <ala.brajner@gmail.com>
2018-01-25 02:43:37 +0100

    zadanie 11.1

---
+++ 11.1_GenerowanieLiczbDoPliku.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+void GenerowanieTablicyZLiczbami(int wielkoscTablicy, char tablica[wielkoscTablicy])
+{
+	for(int i = 0; i<=wielkoscTablicy; i++)
+  	{
+    		tablica[i] = rand()%1000;
+  	}
+}
+
+int main(int argc, char **argv)
+{
+	int rozmiarTablicy = *argv[2];
+	const char* nazwaPliku = argv[1];
+	FILE * plik = fopen(nazwaPliku, "w");
+
+	srand(time(NULL));
+	char tablica[rozmiarTablicy];
+	GenerowanieTablicyZLiczbami(rozmiarTablicy, tablica);
+
+	fprintf(plik, "%s", tablica);
+	fclose(plik);
+	return 0;
+}
