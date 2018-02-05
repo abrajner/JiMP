b2e4e68bd5e1e02aaf76340eab1248812b213a82
abrajner <ala.brajner@gmail.com>
2018-02-05 23:39:13 +0100

    poprawa 11.1

--- 11.1_GenerowanieLiczbDoPliku.c
+++ 11.1_GenerowanieLiczbDoPliku.c
@@ -2,34 +2,42 @@
 #include <time.h>
 #include <stdlib.h>
 
-void GenerowanieTablicyZLiczbami(int wielkoscTablicy, unsigned tablica[wielkoscTablicy], FILE* plik)
+void GenerowanieTablicyZLiczbami(int wielkoscTablicy, FILE* plik)
 {
-	for(int i = 0; i<=wielkoscTablicy; i++)
+	for(int i = 0; i < wielkoscTablicy; i++)
   	{
-		tablica[i] = rand()%1000;
-		fprintf(plik, " %d ", tablica[i]);
+		fprintf(plik, " %d ", rand()%1000);
   	}
 }
 
 int main(int argc, char **argv)
-{
-	int rozmiarTablicy = *argv[2];
-	const char* nazwaPliku = argv[1];
-	FILE * plik = fopen(nazwaPliku, "w");
-	unsigned tablica[rozmiarTablicy];	
+{	
+	srand(time(NULL));
+	FILE * plik = NULL;
+	int rozmiarTablicy = 0;
+	const char* nazwaPliku = NULL;
 
 	if(argc < 3)
 	{
 		printf("Nie wpisales wszystkich argumentow\n");
-		exit (-1);
+		return -1;
 	}
-
-	srand(time(NULL));
-
-	if (NULL == plik)
+	else 
+	{
+		rozmiarTablicy = *argv[2];
+		nazwaPliku = argv[1];
+		plik = fopen(nazwaPliku, "w");
+	}
+	
+	if(rozmiarTablicy < 0)
+	{
+		printf("Rozmiar tablicy nie moze byc mniejszy od 0!\n");
+		return -1;
+	}
+	else if (NULL == plik)
 		return -1;
 	else
-		GenerowanieTablicyZLiczbami(rozmiarTablicy, tablica, plik);
+		GenerowanieTablicyZLiczbami(rozmiarTablicy, plik);
 
 	fclose(plik);
 	return 0;
